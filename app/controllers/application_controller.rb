cecd1e37beec3fb12779eb199c743add84d079ee
Whitny Edwards <wne4e2@gmail.com>
2019-03-23 18:57:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,14 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-   user = User.new(:username => params[:username], :password => params[:password])
-  if user.save
-    redirect "/login"
-  else
-    redirect "/failure"
-end
-	end
-
+    #your code here
   end
 
   get '/account' do
